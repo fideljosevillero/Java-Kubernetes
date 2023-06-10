64e297d9515ba237375dd436570620850e225a1b
fidel <fideljose1708@hotmail.com>
2023-06-10 02:08:07 -0500

    Saving course data - Course - DB Postgresql

--- Course/src/main/java/com/fideljose/Course/controller/CourseController.java
+++ Course/src/main/java/com/fideljose/Course/controller/CourseController.java
@@ -1,9 +1,13 @@
 package com.fideljose.Course.controller;
 
+import com.fideljose.Course.entity.model.Course;
 import com.fideljose.Course.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +19,10 @@ public class CourseController {
     @GetMapping("/")
     public ResponseEntity<?> getCourses(){
         return ResponseEntity.ok(service.listCourse());
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> saveCourse(@RequestBody Course course){
+        return ResponseEntity.ok().body(service.saveCourse(course));
     }
 }
--- Course/src/main/java/com/fideljose/Course/entity/model/Course.java
+++ Course/src/main/java/com/fideljose/Course/entity/model/Course.java
@@ -1,16 +1,26 @@
 package com.fideljose.Course.entity.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "course")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
     private String name;
 
+    @NotEmpty
     private String address;
 }
