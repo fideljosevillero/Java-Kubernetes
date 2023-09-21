3c5c4f675e9ac3a7980bc5786da0f4c2702c752f
fidel <fideljose1708@hotmail.com>
2023-09-21 00:24:41 -0500

    Implement Spring Cloud Kubernetes

--- student-service/src/main/java/com/fideljose/studentservice/studentservice/StudentServiceApplication.java
+++ student-service/src/main/java/com/fideljose/studentservice/studentservice/StudentServiceApplication.java
@@ -2,8 +2,11 @@ package com.fideljose.studentservice.studentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StudentServiceApplication {
 
